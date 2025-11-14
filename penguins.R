197b9147348977590a596a2e557a71f753b0c20a
youkgreeve <youk.greeve@gu.se>
2025-11-14 10:51:58 +0100

    add lm plot

--- penguins.R
+++ penguins.R
@@ -2,6 +2,7 @@ penguins <- palmerpenguins::penguins
 
 library(ggplot2)
 penguins |> 
-  ggplot(aes(x=species,y=bill_length_mm))+
-  geom_boxplot()+
+  ggplot(aes(x=bill_length_mm,y=bill_depth_mm,color=species))+
+  geom_point()+
+  geom_smooth(method="lm")+
   theme_bw()
